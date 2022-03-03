32dc0e807e4479f45843a8d3fdc0e551cf751c61
Raj Kumar Pathak <93421554+Rajpathakcu@users.noreply.github.com>
2022-03-03 12:08:59 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,3 @@
+# recognize speech using IBM Speech to Text
+APIKEY = "sj1-3ugteVbYkXyC3HKKYbx0_MsDpepu3PLrhHJHn2hk"  
+URL = "https://api.eu-gb.speech-to-text.watson.cloud.ibm.com/instances/12e6ead4-284a-4b41-bb8e-d8150b88a5d6" 
